d8cd3f14c0c927406d9a05b9fee60da678eee2dc
Viktor Khristenko <vdkhristenko1991@gmail.com>
2019-02-13 13:35:17 +0100

    vadd ready, now profiling

--- common/utils.h
+++ common/utils.h
@@ -1,10 +1,27 @@
 #ifndef common_h
 #define common_h
 
+#include <string>
+
+#include <cuda.h>
+
 namespace cuda {
 
+inline
+void assert_if_error(std::string const& msg) {
+    auto check = [](auto code) {
+        if (code != cudaSuccess) {
+            std::cout << "\t\t" << cudaGetErrorString(code) << std::endl;
+            //assert(false);
+        }   
+    };  
+
+    std::cout << msg << std::endl;
+    check(cudaGetLastError());
+}
+
 template<typename T>
-void cuda_malloc(T *ptr, size_t size) {
+void cuda_malloc(T *&ptr, size_t size) {
     cudaMalloc((void**)&ptr, sizeof(T) * size);
 }
 
@@ -14,8 +31,8 @@ void copy_to_dev(T *dest, T const*src, size_t size) {
 }
 
 template<typename T>
-void copy_to_dev(T *dest, std::vector<T> const& src, size_t size) {
-    copy_to_dev<T>(dest, src.data(), size);
+void copy_to_dev(T *dest, std::vector<T> const& src) {
+    copy_to_dev<T>(dest, src.data(), src.size());
 }
 
 template<typename T>
@@ -24,8 +41,24 @@ void copy_to_host(T *dest, T const* src, size_t size) {
 }
 
 template<typename T>
-void copy_to_host(std::vector<T> &dest, T const* src, size_t size) {
-    copy_to_host<T>(dest.data(), src, size);
+void copy_to_host(std::vector<T> &dest, T const* src) {
+    copy_to_host<T>(dest.data(), src, dest.size());
+}
+
+}
+
+namespace validation {
+
+template<typename T>
+bool validate_vectors(std::vector<T> const& a, std::vector<T> const& b) {
+    if (a.size() != b.size())
+        return false;
+    bool result = true;
+    for (unsigned int i=0; i<a.size(); i++) {
+//        std::cout << a[i] << "  " << b[i] << std::endl;
+        result &= a[i] == b[i];
+    }
+    return result;
 }
 
 }
--- vadd/vadd.cu
+++ vadd/vadd.cu
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cassert>
 #include "../common/utils.h"
 
 template<typename T>
@@ -12,28 +14,44 @@ void kernel_vadd(T const* as, T const* bs, T *cs, unsigned int size) {
 
 template<typename T>
 void test_add(unsigned int size) {
-    std::vector<T> h_as, h_bs, h_cs;
-    T *d_as, *d_bs, *d_cs;
+    std::vector<T> h_as(size), h_bs(size), h_cs(size, 0), result(size);
+    T *d_as{nullptr}, *d_bs{nullptr}, *d_cs{nullptr};
 
     // allocate
-    cuda::cuda_malloc(d_as);
-    cuda::cuda_malloc(d_bs);
-    cuda::cuda_malloc(d_cs);
+    cuda::cuda_malloc(d_as, size);
+    cuda::cuda_malloc(d_bs, size);
+    cuda::cuda_malloc(d_cs, size);
+    cuda::assert_if_error("checking cuda mallocs");
+
+    // fill the input vectors
+    for (unsigned int i=0; i<size; i++) {
+        h_as[i] = T{static_cast<int>(i)};
+        h_bs[i] = T{static_cast<int>(i)};
+        result[i] = h_as[i] + h_bs[i];
+    }
 
     // copy to device
-    cuda::copy_to_dev(d_as, h_as, size);
-    cuda::copy_to_dev(d_bs, h_bs, size);
+    cuda::copy_to_dev(d_as, h_as);
+    cuda::copy_to_dev(d_bs, h_bs);
+    cuda::assert_if_error("checking copying host to device");
 
     int nthreads_per_block = 256;
     int nblocks = (size + nthreads_per_block - 1) / nthreads_per_block;
     kernel_vadd<T><<<nblocks, nthreads_per_block>>>(d_as, d_bs, d_cs, size);
+    cuda::assert_if_error("checking vadd kernel");
 
     // copy back
-    cuda::copy_to_host(h_cs, d_cs, size);
+    cuda::copy_to_host(h_cs, d_cs);
+    cuda::assert_if_error("checking copying device to host");
+
+    // validate
+    auto is_valid = validation::validate_vectors(result, h_cs);
+    assert(is_valid && "incorrect output from vadd kernel");
+    std::cout << "results valid" << std::endl;
 }
 
 int main(int argc, char** argv) {
-    int size = std::stoi(argv[1]);
+    int size = std::atoi(argv[1]);
     test_add<int>(size);
     return 0;
 }
