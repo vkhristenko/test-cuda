c73fc3f9e9202b19f6e60ac608ccee8e8a30e1ed
vkhristenko <vdkhristenko1991@gmail.com>
2019-05-29 21:16:38 +0200

    inlining the fnnls

--- fnnls/main.cu
+++ fnnls/main.cu
@@ -4,7 +4,7 @@
 
 #include "../common/utils.h"
 #include "include/inplace_fnnls.h"
-#include "include/fnnls_no_mm_mv.h"
+//#include "include/fnnls_no_mm_mv.h"
 
 template<typename T>
 std::vector<vector_t<T>> run_cpu(std::vector<matrix_t<T>> const& As, 
@@ -93,11 +93,97 @@ void kernel_fnnls(
         vector_t<T> * __restrict__ Atbs,
         vector_t<T> * __restrict__ xs,
         unsigned int n) {
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int const tid = threadIdx.x + blockIdx.x * blockDim.x;
 
     if (tid >= n) return;
 
-    v2::fnnls(AtAs[tid], Atbs[tid], xs[tid]);
+    constexpr double eps = 1e-11;
+    constexpr unsigned int max_iterations = 1000;
+    auto& AtA = AtAs[tid];
+    auto& Atb = Atbs[tid];
+    auto& x = xs[tid];
+    using data_type = T;
+
+    auto nPassive = 0;
+    // main loop
+    for (auto iter = 0; iter < max_iterations; ++iter) {
+        const auto nActive = VECTOR_SIZE - nPassive;
+
+        if(!nActive)
+          break;
+
+        //  
+        unsigned int w_max_idx = -1;
+        auto max_w {static_cast<T>(-1)};
+        for (unsigned int i=VECTOR_SIZE-nActive; i<VECTOR_SIZE; i++) {
+            auto sum_per_row{static_cast<T>(0)};
+            auto const atb = Atb(i);
+            #pragma unroll
+            for (unsigned int k=0; k<VECTOR_SIZE; k++)
+                sum_per_row += AtA(i, k) * x(k);
+
+            // compute gradient value and check if it is greater than max
+            auto const wvalue = atb - sum_per_row;
+            if (max_w < wvalue) {
+                max_w = wvalue;
+                w_max_idx = i;
+            }
+        }
+
+        // check for convergence
+        if (max_w < eps)
+          break;
+
+        // swap AtA to avoid copy
+        AtA.col(nPassive).swap(AtA.col(w_max_idx));
+        AtA.row(nPassive).swap(AtA.row(w_max_idx));
+        // swap Atb to match with AtA
+        Eigen::numext::swap(Atb.coeffRef(nPassive), Atb.coeffRef(w_max_idx));
+        Eigen::numext::swap(x.coeffRef(nPassive), x.coeffRef(w_max_idx));
+
+        ++nPassive;
+
+        // inner loop
+        vector_t<data_type> s;
+        while (nPassive > 0) {
+          s.head(nPassive) =
+              AtA.topLeftCorner(nPassive, nPassive).llt().solve(Atb.head(nPassive));
+
+          if (s.head(nPassive).minCoeff() > 0.) {
+            x.head(nPassive) = s.head(nPassive);
+            break;
+          }
+
+          auto alpha = std::numeric_limits<double>::max();
+          Index alpha_idx = 0;
+
+          for (auto i = 0; i < nPassive; ++i) {
+            if (s[i] <= 0.) {
+              auto const ratio = x[i] / (x[i] - s[i]);
+              if (ratio < alpha) {
+                alpha = ratio;
+                alpha_idx = i;
+              }
+            }
+          }
+
+          if (std::numeric_limits<double>::max() == alpha) {
+            x.head(nPassive) = s.head(nPassive);
+            break;
+          }
+
+          x.head(nPassive) += alpha * (s.head(nPassive) - x.head(nPassive));
+          x[alpha_idx] = 0;
+          --nPassive;
+
+          AtA.col(nPassive).swap(AtA.col(alpha_idx));
+          AtA.row(nPassive).swap(AtA.row(alpha_idx));
+          // swap Atb to match with AtA
+          Eigen::numext::swap(Atb.coeffRef(nPassive), Atb.coeffRef(alpha_idx));
+          Eigen::numext::swap(x.coeffRef(nPassive), x.coeffRef(alpha_idx));
+
+    }
+  }
 }
 
 template<typename T>
