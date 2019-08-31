fc77be45992b9c56c96e45be3f89e4882823a61b
Viktor Khristenko <vdkhristenko1991@gmail.com>
2019-08-31 14:38:53 +0200

    cooperative groups cholesky decomp implementation

--- cholesky/main.cu
+++ cholesky/main.cu
@@ -7,89 +7,15 @@
 #include "../common/utils.h"
 #include "parse_matrices.hpp"
 
+#include <cooperative_groups.h>
+using namespace cooperative_groups;
+
 /*
 constexpr unsigned long MATRIX_SIZE = 3;
 
 template<typename T>
 using matrix_t = Eigen::Matrix<T, MATRIX_SIZE, MATRIX_SIZE, Eigen::ColMajor>;
 */
-
-namespace mymath {
-
-template<typename T>
-struct FusedCholeskyForwardSubst {
-    __forceinline__
-    __device__ static void compute(
-            matrix_t<T> const& M, 
-            vector_t<T> const& b,
-            matrix_t<T> &L,
-            vector_t<T> &intermediate,
-            int view) {
-        // compute element 0,0 for L
-        auto const sqrtm_0_0 = std::sqrt(M(0, 0));
-        L(0, 0) = sqrtm_0_0;
-
-        // compute solution for forward subst for element 0
-        auto const interm_0 = b(0) / sqrtm_0_0;
-        intermediate(0) = interm_0;
-
-        for (int i=1; i<view; ++i) {
-            // load the value to sub from
-            T total = b(i);
-
-            // first compute elements to the left of the diagoanl
-            T sumsq{static_cast<T>(0)};
-            for (int j=0; j<i; ++j) {
-                T sumsq2{static_cast<T>(0)};
-                auto const m_i_j = M(i, j);
-                for (int k=0; k<j; ++k)
-                    sumsq2 += L(i, k) * L(j, k);
-
-                // comput the i,j : i>j, elements to the left of the diagonal
-                auto const value_i_j = (m_i_j - sumsq2) / L(j, j);
-                L(i, j) = value_i_j;
-
-                // needed to compute diagonal element
-                sumsq += value_i_j * value_i_j;
-
-                total -= value_i_j * intermediate(j);
-            }
-
-            // second, compute the diagonal element
-            auto const l_i_i = std::sqrt(M(i, i) - sumsq);
-            L(i, i) = l_i_i;
-
-            intermediate(i) = total / l_i_i;
-        }
-    }
-};
-
-//
-// note, we need to use transpose of M
-//
-template<typename T>
-struct BackwardSubst {
-    __forceinline__
-    __device__ static void compute(
-            matrix_t<T> const& M,
-            vector_t<T> const& b,
-            vector_t<T> &x,
-            int view) {
-        // first element
-        x(view - 1) = b(view - 1) / M(view-1, view-1);
-
-        // the rest
-        for (int i=view-2; i>=0; --i) {
-            T total{static_cast<T>(0)};
-            for (int j=i+1; j<view; ++j) 
-                total += M(j, i) * x(j);
-
-            x(i) = (b(i) - total) / M(i, i);
-        }
-    }
-};
-
-}
 
 namespace cpu_based { 
 
@@ -102,6 +28,178 @@ void kernel_cholesky(matrix_t<T> const* As,
     if (idx < n) {
         Ls[idx] = As[idx].llt().matrixL();
     }
+}
+
+}
+
+namespace gpu_v1 {
+
+constexpr int threadTileSize = 16;
+
+template<typename T>
+__global__
+void kernel_cholesky(matrix_t<T> const* As, matrix_t<T> *Ls) {
+    // assume that we 
+    constexpr auto N = matrix_t<T>::RowsAtCompileTime;
+    static_assert(N <= threadTileSize);
+
+    // x <- [0, 15]
+    // y <- [0, nchannels per block)
+    int const ch = threadIdx.y;
+
+    // partition all threads into groups of 16(threadTileSize)
+    auto this_tile = tiled_partition<threadTileSize>(this_thread_block());
+    int const rank = this_tile.thread_rank();
+
+    // load M(i, 0)
+    T m_i_0;
+    if (rank<N) m_i_0 = As[ch](rank, 0);
+
+    // compute L(0, 0)
+    T l_0_0;
+    if (rank==0) {
+        l_0_0 = std::sqrt(m_i_0);
+
+        // store this guy to global
+        Ls[ch](0, 0) = l_0_0;
+    }
+    // retrieve L(0, 0) from rank 0 thread 
+    l_0_0 = this_tile.shfl(l_0_0, 0);
+    // compute L(i, 0) for i>=1
+    T l_i_0;
+    if (rank>0 && rank<N) {
+        l_i_0 = m_i_0 / l_0_0;
+
+        // store this guy to global L(i, 0) for i >= 1
+        Ls[ch](rank, 0) = l_i_0;
+    }
+
+    // accumulators
+    // FIXME: this must be in registers
+    T reg_sumsq;
+    T reg_accum[N-2];
+
+    // accumulate per row
+    if (rank>=1 && rank<N)
+        reg_sumsq = l_i_0 * l_i_0;
+
+    // accumulate over column 0
+    #pragma unroll
+    for (int ineighbor=1; ineighbor<N-1; ineighbor++) {
+        auto value_l_i_0 = this_tile.shfl_up(l_i_0, ineighbor);
+        if (rank>ineighbor && rank<N)
+            reg_accum[rank-ineighbor-1] = value_l_i_0 * l_i_0;
+    }
+
+    // iterate until the last one:
+    // for icol>=1
+    // - load icol
+    // - compute L(icol, icol)
+    // - compute L(i, icol) for i>icol
+    // - accumulate for icol
+    #pragma unroll
+    for (int icol=1; icol<N-1; icol++) {
+        // load icol
+        T m_i_icol;
+        if (rank>=icol && rank<N)
+            m_i_icol = As[ch](rank, icol);
+
+        // compute L(icol, icol)
+        T l_icol_icol;
+        if (rank==icol) {
+            l_icol_icol = std::sqrt(m_i_icol - reg_sumsq);
+
+            // store to global
+            Ls[ch](icol, icol) = l_icol_icol;
+        }
+
+        // broadcast icol thread value and sync
+        l_icol_icol = this_tile.shfl(l_icol_icol, icol);
+
+        // compute L(i, icol) for i>icol
+        T l_i_icol;
+        if (rank>icol && rank<N) {
+            l_i_icol = (m_i_icol - reg_accum[icol-1]) / l_icol_icol;
+
+            // store to global
+            Ls[ch](rank, icol) = l_i_icol;
+        }
+
+        // accumulate per row
+        if (rank>=icol+1 && rank<N)
+            reg_sumsq += l_i_icol*l_i_icol;
+
+        // accumulate over column icol
+        auto Nneighbors = N-icol-1;
+        #pragma unroll
+        for (int delta=1; delta<Nneighbors; delta++) {
+            auto value_l_i_icol = this_tile.shfl_up(l_i_icol, delta);
+            if (rank>icol+delta && rank<N)
+                reg_accum[rank-delta-1] += value_l_i_icol*l_i_icol;
+        }
+    }
+
+    // compute the L(N-1, N-1)
+    if (rank==N-1) {
+        auto m_i_i = As[ch](N-1, N-1);
+        auto l_i_i = std::sqrt(m_i_i - reg_sumsq);
+
+        // store to global
+        Ls[ch](N-1, N-1) = l_i_i;
+    }
+}
+
+template<typename T>
+std::vector<matrix_t<T>> launcher(std::vector<matrix_t<T>> const& As) {
+    std::vector<matrix_t<T>> results(As.size());
+    std::cout << "gpu v1\n";
+    
+    // devcie ptrs
+    matrix_t<T> *d_As;
+    matrix_t<T> *d_Ls;
+    unsigned int n = As.size();
+
+    cudaEvent_t start;
+    cudaEvent_t end;
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&end);
+
+    // allocate
+    cuda::cuda_malloc(d_As, n);
+    cuda::cuda_malloc(d_Ls, n); 
+    cuda::assert_if_error("\tcuda mallocs");
+
+    // copy
+    cuda::copy_to_dev(d_As, As);
+    cuda::assert_if_error("\tcuda memcpy to device");
+
+    {
+        int nchannelsPerBlock = 32;
+        dim3 nthreads = {threadTileSize, nchannelsPerBlock};
+        int blocks{(As.size() + nchannelsPerBlock - 1) / nchannelsPerBlock};
+        kernel_cholesky<T><<<blocks, nthreads>>>(
+            d_As, d_Ls);
+        cudaDeviceSynchronize();
+        cuda::assert_if_error("\tkernel gpu v1 cholesky decomposition");
+
+        cudaEventRecord(start, 0);
+        kernel_cholesky<T><<<blocks, nthreads>>>(
+            d_As, d_Ls);
+        cudaEventRecord(end, 0);
+        cudaEventSynchronize(end);
+        float ms;
+        cudaEventElapsedTime(&ms, start, end);
+        cuda::assert_if_error("\tkernel gpu v1 cholesky decomposition");
+        printf("\telapsed time = %f (ms)\n", ms);
+    }
+    cuda::copy_to_host(results, d_Ls);
+    cuda::assert_if_error("\tcuda memcpy back to host");
+
+    cudaFree(d_As);
+    cudaFree(d_Ls);
+
+    return results;
 }
 
 }
@@ -358,6 +456,7 @@ int main(int argc, char** argv) {
     std::cout << "start computing\n";
     auto result_cpubased = cpu_based_gpu(As);
     auto result_gpu_v0 = wrapper_gpu_v0(As);
+    auto result_gpu_coopgroups = gpu_v1::launcher(As);
 
     std::cout << "comparison\n";
     /*
@@ -377,6 +476,7 @@ int main(int argc, char** argv) {
 
     auto vals_cpu_cpubased = validation::validate_eigen_vectors(cpu, result_cpubased) ;
     auto vals_cpu_gpuv0 = validation::validate_eigen_vectors(cpu, result_gpu_v0);
+    auto vals_cpu_gpuv1 = validation::validate_eigen_vectors(cpu, result_gpu_v0);
     if (vals_cpu_cpubased.size() > 0) {
         std::cout << "--- cpu vs gpu cpu based ---\n";
     } else {
@@ -386,6 +486,10 @@ int main(int argc, char** argv) {
         std::cout << "--- cpu vs gpu v0 ---\n";
     } else 
         std::cout << "+++ cpu vs gpu v0 +++\n";
+    if (vals_cpu_gpuv1.size() > 0) {
+        std::cout << "--- cpu vs gpu v1 ---\n";
+    } else 
+        std::cout << "+++ cpu vs gpu v1 +++\n";
     
     return 0;
 }
